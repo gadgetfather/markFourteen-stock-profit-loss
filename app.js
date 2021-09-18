9575d90e7c8eabb22b0379cda5b8a7a54b6ba77c
gadgetfather <kamblea635@gmail.com>
2021-09-18 10:48:56 +0530

    selecting rem elements

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var intialPrice = document.querySelector('#price1')
 var noOfStocks = document.querySelector('#no-of-stocks')
 var currentPrice = document.querySelector('#current-price')
+var submitBtm = document.querySelector('#output-btn')
+var OutputEl = document.querySelector('#output')
 
