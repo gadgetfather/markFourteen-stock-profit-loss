ec3eb092895b689ed51e29e4d9b7d089392471c7
gadgetfather <kamblea635@gmail.com>
2021-09-18 11:00:47 +0530

    building processing part

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ var currentPrice = document.querySelector('#current-price')
 var submitBtm = document.querySelector('#output-btn')
 var OutputEl = document.querySelector('#output')
 
+
+function calculateProfitLoss(initial,quantity,current){
+    if(initial> current){
+        var loss = (initial-current)*quantity
+        var lossPercent = (loss/initial)*100
+    }else if (current > initial){
+        var profit = (current-initial)*quantity
+        var profitPercentage = (profit/initial)*100
+        console.log(`profit is ${profit} and ${profitPercentage}`)
+
+    }
+    else{
+
+    }
+
+}
+calculateProfitLoss(10,10,12)
