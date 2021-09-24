bb9dfd47513e1b51ba11cf3cc175cf0763bfbd52
gadgetfather <kamblea635@gmail.com>
2021-09-25 00:08:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,40 @@ var currentPrice = document.querySelector('#current-price')
 var submitBtm = document.querySelector('#output-btn')
 var OutputEl = document.querySelector('#output')
 
+intialPrice.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || (e.keyCode == 8)
+      || (e.keyCode == 110)
+      || e.keyCode == 190)) {
+        return false;
+    }
+}
+
+currentPrice.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || (e.keyCode == 8)
+      || (e.keyCode == 110)
+      || e.keyCode == 190)) {
+        return false;
+    }
+}
+
+noOfStocks.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
+
+
 function submitHandle(){
     var ip =Number( intialPrice.value)
     var nos=Number( noOfStocks.value)
     var cp = Number(currentPrice.value)
-    calculateProfitLoss(ip,cp)
+    calculateProfitLoss(ip,nos,cp)
 }
 
 function financial(x){
@@ -16,22 +45,22 @@ function financial(x){
 }
 
 
-function calculateProfitLoss(initial,current){
+function calculateProfitLoss(initial,quantity,current){
     
     if(intialPrice.value!=""&& noOfStocks.value!=""&&currentPrice!=""){
      
     if(initial> current){
-        var loss =financial((initial-current))
-        var lossPercent =financial((loss/initial)*100)
+        var loss =financial(((initial*quantity)-(current*quantity)))
+        var lossPercent =financial((loss/(initial*quantity))*100)
         OutputEl.style.color="Red"
         OutputEl.style.backgroundColor = "white"
-        OutputEl.textContent=`Your loss is Rs.${loss} and ${lossPercent}%`
+        OutputEl.textContent=`The invested amount is ${(initial*quantity)} Your loss is Rs.${loss} and ${lossPercent}%`
     }else if (current > initial){
-        var profit = financial((current-initial))
-        var profitPercentage = financial((profit/initial)*100)
+        var profit = financial(((current*quantity)-(initial*quantity)))
+        var profitPercentage = financial((profit/(initial*quantity))*100)
         OutputEl.style.color="green"
         OutputEl.style.backgroundColor = "white"
-        OutputEl.textContent=(`Profit is Rs.${profit} and ${profitPercentage}%`)
+        OutputEl.textContent=(`The invested amount is ${(initial*quantity)}Profit is Rs.${profit} and ${profitPercentage}%`)
 
     }
     else{
